7c682cde881507541352a81ec23df1d60f4a3125
Abdul Zamani <azamani2@ucmerced.edu>
2020-06-09 14:31:28 -0700

    Arrays: Q9-11

--- exVecCircle.f90
+++ exVecCircle.f90
@@ -1,17 +1,17 @@
       program exVecCircle
       implicit none
-      real :: a, b, c, x, y, z
-      real, dimension(3) :: vec1, vec2, sumvecc
-      real, dimension(3) :: sumvc
+      real :: a, b, x, y
+      real, dimension(2) :: vec1, vec2, sumvecc
+      real, dimension(2) :: sumvc
       real :: dist, areacirc
       !You must define the input variables prior to defining the arrays
       
       write(*,*)'Enter the elements for vector #1: '
-      read*,a,b,c
+      read*,a,b
       write(*,*)'Enter the elements for vector #2: '
-      read*,x,y,z
-      vec1=(/ a,b,c/)
-      vec2=(/x,y,z/) 
+      read*,x,y
+      vec1=(/a,b/)
+      vec2=(/x,y/) 
       !
       !You must define the arrays in terms of the fxns
       !  
@@ -28,9 +28,9 @@
       !Subroutine should be defined in terms of another expression
        function sumvec(vec1,vec2) result(sumvc) 
         integer :: i
-        real, dimension(3), intent(in) :: vec1
-        real, dimension(3), intent(in) :: vec2
-        real, dimension(3)             :: sumvc
+        real, dimension(2), intent(in) :: vec1
+        real, dimension(2), intent(in) :: vec2
+        real, dimension(2)             :: sumvc
         do i =1,3
         sumvc(i) = vec1(i) + vec2(i)
         enddo
@@ -39,17 +39,15 @@
 
       function distance1(vec1) result(dist1)
         real:: dist1
-        real, dimension(3) :: vec, vec1, vec2
-        dist1 = sqrt(((vec1(1))**2)+((vec1(2))**2) &
-        +((vec1(3)**2)))
+        real, dimension(2) :: vec, vec1, vec2
+        dist1 = sqrt(((vec1(1))**2)+((vec1(2))**2))
         return
       end function distance1
 
       function distance2(vec1,vec2) result(dist2)
         real:: dist2
-        real, dimension(3) :: vec, vec1, vec2
-        dist2 = sqrt(((vec1(1)-vec2(1))**2)+((vec1(2)-vec2(2))**2) &
-        +((vec1(3)-vec2(3))**2))
+        real, dimension(2) :: vec, vec1, vec2
+        dist2 = sqrt(((vec1(1)-vec2(1))**2)+((vec1(2)-vec2(2))**2))
         return
       end function distance2
 
